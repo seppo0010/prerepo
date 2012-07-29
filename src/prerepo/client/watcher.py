e88d27bbaf10cab92c15dceee922eeda64410b47
Sebastian Waisbrot <seppo@simpler.co>
2012-07-29 16:00:47 -0300

    Initial snapshot

--- src/prerepo/client/watcher.py
+++ src/prerepo/client/watcher.py
@@ -1,15 +1,17 @@
+import os
 import time
 
 from watchdog.observers import Observer
 from watchdog.events import LoggingEventHandler
+from watchdog.utils.dirsnapshot import DirectorySnapshot
 
 from prerepo.client.models import File
 
 
 class PrerepoEventHandler(LoggingEventHandler):
-    def __init__(self, uid=None, server=None, path=None):
+    def __init__(self, f=None, path=None):
         super(PrerepoEventHandler, self).__init__()
-        self.f = File(uid=uid, server=server)
+        self.f = f
         self.path = path
 
     def get_remote_path(self, path):
@@ -26,7 +28,6 @@ class PrerepoEventHandler(LoggingEventHandler):
     def on_moved(self, event):
         self.f.rename(self.get_remote_path(event.src_path),
             self.get_remote_path(event.dest_path))
-        # TODO: rename files on move directories
 
     def on_deleted(self, event):
         self.f.delete(self.get_remote_path(event.src_path))
@@ -44,13 +45,22 @@ class Watcher(object):
         self.path = path
         self.uid = uid
         self.server = server
+        self.f = File(uid=uid, server=server)
 
     def watch(self):
-        event_handler = PrerepoEventHandler(uid=self.uid, server=self.server,
-                path=self.path)
+        def create(path, stat_info):
+            remote_path = path[len(self.path) - 1:]
+            if os.path.isdir(path):
+                self.f.createdir(remote_path)
+            else:
+                with open(path, 'r') as fp:
+                    self.f.createfile(remote_path, fp.read())
+
+        event_handler = PrerepoEventHandler(f=self.f, path=self.path)
         observer = Observer()
         observer.schedule(event_handler, path=self.path, recursive=True)
         observer.start()
+        DirectorySnapshot(self.path, recursive=True, walker_callback=create)
         try:
             while True:
                 time.sleep(1)
